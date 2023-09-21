c8d52aca4fe77b7e62194ceb07563755c9f949e9
João Dutra <joaovitordutrag@gmail.com>
2023-09-21 12:38:40 -0300

    chore(readme): update file with new changes

--- src/services/account.service.test.ts
+++ src/services/account.service.test.ts
@@ -8,7 +8,7 @@ describe('AccountService', () => {
     })
 
     it('should generate an Ethereum address from a valid public key', () => {
-        const publicKey = '308184020100301006072a8648ce3d020106052b8104000a046d306b0201010420ec87829b77987289c2645a5db1c82850f268e151a2793c192956c4ede0f3ad3ca1440342000402929851092555d7fb07e14ec93635f2547d0d9a5e9e6bb08ec52f8b0d9e6c01b95eb7bb170c24b1e966a4834d9051da8f42b94131523b99e823537d4afd8d9a'
+        const publicKey = '6ce4d193703c268ae99151c58558d49798b41edebdeca82b3b0e2565461fbfa4162bfa79d4ccb1cd8d5dc2e816e0dff58cfe7d399d1017019fe2db22a1544cda'
         const address = accountService.getByPublicKey(publicKey)
         
         expect(address).toMatch(/^0x[a-fA-F0-9]{40}$/)
